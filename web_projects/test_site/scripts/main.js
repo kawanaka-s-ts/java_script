160ca4d125ab4f90a837f7eeae76c5d83283d8de
kawanaka <kawanaka_s@tenso.com>
2019-04-17 19:01:57 +0900

    名前の入力と画像の切り替えを追加

--- web_projects/test_site/scripts/main.js
+++ web_projects/test_site/scripts/main.js
@@ -1,2 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
+function setUserName() {
+  var myName = prompt('あなたの名前を入力してください。');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'TALT はすばらしいよ、' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'TALT はすばらしいよ、' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
